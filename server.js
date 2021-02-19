ca8817d81fab46cecf6c231f223c3462d74e08b6
laurentsem <laurent.sem@edu.itescia.Fr>
2021-02-19 02:13:21 +0100

    Fix

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const cors = require('cors');
 
 // Create Express app
 const app = express()
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:4000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 // CORS
 app.use(cors({origin: '*'}));
