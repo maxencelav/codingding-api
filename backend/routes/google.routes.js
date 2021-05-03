5ae9c94e5790415c575aae4cb38a023dfc0e24b8
laurentsem <laurent.sem@edu.itescia.Fr>
2021-05-03 20:16:23 +0200

    Add JWT. Need to finish it.

--- backend/routes/google.routes.js
+++ backend/routes/google.routes.js
@@ -1,3 +1,5 @@
+var jwt = require('jsonwebtoken')
+
 module.exports = (app) => {
 
     const passport = require("passport");
@@ -15,10 +17,22 @@ module.exports = (app) => {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept", "Authorization");
         res.header("Access-Control-Allow-Credentials", true)
+
+       
+        const token = jwt.sign( {
+              user: req.user,
+            },
+            process.env.JWT_KEY,
+            {
+              expiresIn: "7d"
+            }
+          );
+        res.cookie("token", token, {httpOnly: false})
         res.send(req.user);
     });
 
     app.get("/auth/logout", (req, res) => {
+        res.clearCookie("token");
         req.logout();
         res.send(req.user);
     });
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const session = require('express-session');
+require("dotenv").config();
 
 // Create Express app
 const app = express()
